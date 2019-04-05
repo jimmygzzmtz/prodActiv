67f1c65befa4583ad785f59444a968cec47c392b
Radtke <kevinradtkeg@gmail.com>
2019-04-05 14:05:23 -0600

    .

---
+++ prodActiv/Tag.swift
@@ -0,0 +1,13 @@
+//
+//  Tag.swift
+//  prodActiv
+//
+//  Created by Kevin Radtke on 3/25/19.
+//  Copyright © 2019 Jaime Alberto Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class Tag: NSObject {
+
+}
